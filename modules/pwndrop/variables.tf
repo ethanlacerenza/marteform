512339721c35c5d8f7d13d23b251b2a94239346c
ethanlacerenza <71321892+ethanlacerenza@users.noreply.github.com>
2024-04-09 14:30:44 +0200

    adding directory

---
+++ modules/pwndrop/variables.tf
@@ -0,0 +1,8 @@
+# -------------------------------------------------------------
+# Marsform: Red Teaming Infrastructure Deployment System
+# Developed by Ethan Lacerenza
+#
+# Terraform-based system for deploying infrastructure tailored
+# for red teaming operations. Includes modularized modules for
+# various red teaming services.
+# -------------------------------------------------------------
